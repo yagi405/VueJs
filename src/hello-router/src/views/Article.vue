e58080cbd2b013a2605e1f153a13d8d2e170065e
h-saito <h-saito@powersolutions.co.jp>
2020-12-15 16:17:55 +0900

    add hello-router project

---
+++ src/hello-router/src/views/Article.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="article">
+        <h1>This is an article page</h1>
+        <span> ArticleID : {{id}} </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Article',
+    props:['id'],
+}
+</script>
