1bb94ce0f26c6cdb397ea83401a91d0c4311df97
h-saito <h-saito@powersolutions.co.jp>
2020-12-23 11:21:01 +0900

    add counter

--- src/hello-vuex/src/App.vue
+++ src/hello-vuex/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <input type="button" v-on:click="ondecrement" value="-" />
+  {{count}}
+  <input type="button" v-on:click="onincrement" value="+" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  computed:{
+    count(){
+      return this.$store.state.count
+    },
+  },
+  methods:{
+    onincrement(){
+      this.$store.commit('increment')
+    },
+    ondecrement(){
+      this.$store.commit('decrement')
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/hello-vuex/src/store/index.js
+++ src/hello-vuex/src/store/index.js
@@ -2,8 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    count: 0
   },
   mutations: {
+    increment(state){
+      state.count += 1
+    },
+    decrement(state){
+      state.count -= 1
+    }
   },
   actions: {
   },
